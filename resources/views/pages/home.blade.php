315c1873f8b8405901d1104a68fb0f4c5ef2c49e
Michelazani <michi.zani220@gmail.com>
2024-01-31 17:23:28 +0100

    modifica in home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,4 @@
-<!-- riempie il file app contenuto nella cartella layouts con quello scritto all'interno di @section -->
+<!-- riempie il file app contenuto nella cartella layouts con quello scritto all'interno di section -->
 @extends('layouts.app')
 
 @section('main-content')
