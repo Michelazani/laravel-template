ad25cfa0f31f3d27bb21700a813992910649c498
Michelazani <michi.zani220@gmail.com>
2024-01-31 16:12:24 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import './bootstrap';
+
+// importo per prendere scss
+import '~resources/scss/app.scss';
+
+// importare bootstrap
+import * as bootstrap from 'bootstrap';
+
+// per importare img
+import.meta.glob([
+    '../img/**'
+]); 
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    resolve: {
+        alias: {
+            // modo per dire che quando scrivo ~resourceUsage, sto andando in resources
+            '~resources': '/resources/'
+        }
+    },
+});
